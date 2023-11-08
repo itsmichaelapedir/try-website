dd0dcb43bb4ce385444edafba21202b6be6226c3
Michaela Pedir <123416434+itsmichaelapedir@users.noreply.github.com>
2023-11-08 15:35:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example of a simple JavaScript function
+function greet() {
+    var name = prompt("What's your name?");
+    alert("Hello, " + name + "! Welcome to our website!");
+}
+
+// Call the function when the page loads
+window.onload = greet;
